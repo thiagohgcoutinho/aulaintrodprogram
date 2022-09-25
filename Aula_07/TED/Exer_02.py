015dee7ae0a1b244bcbbb2103ebabd1fd1f8f415
Thiago Coutinho <2022211510098@iesp.edu.br>
2022-09-25 16:03:33 -0300

    TED

--- Aula_07/TED/Exer_02.py
+++ Aula_07/TED/Exer_02.py
@@ -2,9 +2,10 @@ t = str(" Aula 07 - Exercício 02 - TED ")
 print(f"{t:=^40}")
 nasc = int(input("Qual ano você nasceu? "))
 atual = int(input("Qual o ano atual? "))
-if (atual - nasc) < 16:
-    print(f"Como você tem {(atual - nasc)} anos, você ainda não pode votar.")
-elif 16 <= (atual - nasc) < 18:
-    print(f"Como você tem {(atual - nasc)} anos, você já pode votar.")
+idade = atual - nasc
+if idade < 16:
+    print(f"Como você tem {idade} anos, você ainda não pode votar.")
+elif 16 <= idade < 18:
+    print(f"Como você tem {idade} anos, você já pode votar.")
 else:
-    print(f"Como você tem {(atual - nasc)} anos, você já deve votar.")
+    print(f"Como você tem {idade} anos, você já deve votar.")

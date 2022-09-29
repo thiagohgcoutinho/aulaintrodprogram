b697596fdec52250fcfb802c88db894afbda36b4
Thiago Coutinho <2022211510098@iesp.edu.br>
2022-09-28 21:30:13 -0300

    Aula 08

--- Aula_08/Aula_08_Exerc_05.py
+++ Aula_08/Aula_08_Exerc_05.py
@@ -1,2 +1,29 @@
 t = str(" Aula 08 - Exercício 05 ")
 print(f"{t:=^40}")
+print("")
+grupo1 = []
+grupo2 = []
+grupo3 = []
+grupo4 = []
+num = 100
+while num >= 0:
+    print("Digite um número negativo para encerrar!")
+    num = float(input("Digite um número para lista entre 0 e 100: "))
+    if 0 <= num <= 100:
+        if 0 <= num <= 25:
+            grupo1.append(num)
+        elif 26 <= num <= 50:
+            grupo2.append(num)
+        elif 51 <= num <= 75:
+            grupo3.append(num)
+        else:
+            grupo4.append(num)
+    elif num > 100:
+        print("Número acima de 100 não é permitido!")
+    else:
+        print("Lista encerrada.")
+
+print(f"A quantidade de número entre 0 e 25 foi {len(grupo1)}!")
+print(f"A quantidade de número entre 26 e 50 foi {len(grupo2)}!")
+print(f"A quantidade de número entre 51 e 75 foi {len(grupo3)}!")
+print(f"A quantidade de número entre 76 e 100 foi {len(grupo4)}!")
